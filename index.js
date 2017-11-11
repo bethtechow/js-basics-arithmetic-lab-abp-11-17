0e366ac62aa34426354c854ff362584cb4754f17
bethtechow <bethtechow@yahoo.com>
2017-11-11 14:36:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-let newID=oldID + 1000000000
+let newID=oldID + 1000000000;
 let currentAge = number.isInterger(currentAge);
